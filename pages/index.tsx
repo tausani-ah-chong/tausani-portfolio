caef2c4a1ada38f9600cf2b8f905d54030ef30f0
tausani-ah-chong <tausani93@gmail.com>
2021-03-19 21:49:49 +1300

    add first project devportfolio

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 
 function HomePage(): JSX.Element {
 	return (
@@ -28,20 +29,39 @@ function HomePage(): JSX.Element {
 						<h1 className="text-left text-3xl font-bold">Tausani Ah Chong</h1>
 					</div>
 					<div>
-						<p className="py-6">
-							<span className="font-bold">Hello!</span> I&apos;m a Full Stack Software Developer
-							with a passion for taking a meaningful idea and bringing it to fruition. I’m looking
-							to be part of a team that I can grow with, that builds on ideas that are not just
-							well-designed apps, but into ideas or tools that are meaningful to society.
+						<p className="py-8">
+							<span className="font-bold text-lg">Hello!</span> I&apos;m a Full Stack Software
+							Developer with a passion for taking a meaningful idea and bringing it to fruition. I’m
+							looking to be part of a team that I can grow with, that builds on ideas that are not
+							just well-designed apps, but into ideas or tools that are meaningful to society.
 						</p>
 					</div>
 					<div>
 						<p
-							className="font-bold"
+							className="font-bold text-sm text-gray-600"
 							style={{ textTransform: 'uppercase', letterSpacing: '0.15em' }}
 						>
 							Projects
 						</p>
+					</div>
+					<div>
+						<ul className="py-2">
+							<li className="py-2 list-disc text-lg">
+								<a
+									className="hover:underline py-2 font-bold"
+									href="https://dev-portfolio-21.herokuapp.com"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									DevPortfolio
+								</a>
+								<p className="py-2 text-base">
+									WIP: A platform for developers to be able to showcase their portfolios and connect
+									with employers.
+								</p>
+								<p className="text-xs text-gray-600">March 19, 2021</p>
+							</li>
+						</ul>
 					</div>
 				</section>
 			</div>
